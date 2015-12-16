8e13203b3bd44abd8e192d0a2ccf70edcf394390
Owen-HUANG <huangzhengyuan1993@gmail.com>
2015-12-16 21:52:42 +0800

    1. added main activity with navigation drawer 2. added empty fragment and record fragment with recycle view and some dummy data

--- app/src/main/java/csci3310/cuhk/edu/hk/project/MainActivity.java
+++ app/src/main/java/csci3310/cuhk/edu/hk/project/MainActivity.java
@@ -3,13 +3,25 @@ package csci3310.cuhk.edu.hk.project;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import csci3310.cuhk.edu.hk.project.fragment.EmptyFragment;
+import csci3310.cuhk.edu.hk.project.fragment.RecordFragment;
+import csci3310.cuhk.edu.hk.project.fragment.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, RecordFragment.OnListFragmentInteractionListener {
+
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +29,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Today");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.new_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,12 +40,43 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, EmptyFragment.newInstance("No Record For Today")).commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -48,5 +93,51 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_today) {
+            actionBar.setTitle(getString(R.string.today));
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, EmptyFragment.newInstance("No Record For Today")).commit();
+        } else if (id == R.id.nav_week) {
+            actionBar.setTitle(getString(R.string.week));
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, RecordFragment.newInstance()).commit();
+        } else if (id == R.id.nav_month) {
+            actionBar.setTitle(getString(R.string.month));
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, EmptyFragment.newInstance("No Record For Month")).commit();
+        } else if (id == R.id.nav_year) {
+            actionBar.setTitle(getString(R.string.year));
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, EmptyFragment.newInstance("No Record For Year")).commit();
+        } else if (id == R.id.nav_account) {
+            actionBar.setTitle(getString(R.string.account));
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, EmptyFragment.newInstance("No Record For Account")).commit();
+        } else if (id == R.id.nav_summary) {
+            actionBar.setTitle(getString(R.string.summary));
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, EmptyFragment.newInstance("No Record For Summary")).commit();
+        } else if (id == R.id.nav_budget) {
+            actionBar.setTitle(getString(R.string.budget));
+            // TODO: Retrieve database records to determine which fragment to show
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, EmptyFragment.newInstance("No Record For Budget")).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyRecord item) {
+        Snackbar.make(findViewById(R.id.coordinator_layout_container), item.content, Snackbar.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/csci3310/cuhk/edu/hk/project/fragment/RecordFragment.java
@@ -0,0 +1,87 @@
+package csci3310.cuhk.edu.hk.project.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import csci3310.cuhk.edu.hk.project.R;
+import csci3310.cuhk.edu.hk.project.fragment.dummy.DummyContent;
+import csci3310.cuhk.edu.hk.project.fragment.dummy.DummyContent.DummyRecord;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ */
+public class RecordFragment extends Fragment {
+
+    private OnListFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public RecordFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static RecordFragment newInstance() {
+        RecordFragment fragment = new RecordFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_record_list, container, false);
+
+        // Set the adapter
+        Context context = view.getContext();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.record_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(new MyRecordRecyclerViewAdapter(DummyContent.RECORDS, mListener));
+
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onListFragmentInteraction(DummyRecord item);
+    }
+}
