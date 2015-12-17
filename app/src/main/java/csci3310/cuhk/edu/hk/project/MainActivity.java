aa4165c580bdd065e6180461db544e5ea87a7771
Owen-HUANG <huangzhengyuan1993@gmail.com>
2015-12-17 13:18:46 +0800

    1. created new record activity

--- app/src/main/java/csci3310/cuhk/edu/hk/project/MainActivity.java
+++ app/src/main/java/csci3310/cuhk/edu/hk/project/MainActivity.java
@@ -1,5 +1,6 @@
 package csci3310.cuhk.edu.hk.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -36,8 +37,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, CreateNewRecord.class);
+                startActivity(intent);
             }
         });
 
